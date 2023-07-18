2870265080e591271a8528bd473540b6538d542d
Mekedi1 <135107480+Mekedi1@users.noreply.github.com>
2023-07-18 15:57:25 -0700

    committed

---
+++ src/launchBrowser/GitTest.java
@@ -0,0 +1,13 @@
+package launchBrowser;
+
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("git test");
+		printMe();
+	}
+	public static void printMe () {
+		System.out.println("git test 1");
+		System.out.println("git test 2");
+		System.out.println("git test 3");
+	}
+}
